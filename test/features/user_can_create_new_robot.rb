7bda5b1524ac2d28ffcc8a6286ac7c66c8c21336
Brian Heim <bheim67@gmail.com>
2016-08-17 21:48:28 -0600

    finished prelim feature testing

---
+++ test/features/user_can_create_new_robot.rb
@@ -0,0 +1,26 @@
+require_relative '../test_helper'
+
+class CreateNewRobotTest < FeatureTest
+  def test_user_can_create_robot
+    visit '/'
+    click_link('New Robot')
+
+    assert '/robots/new', current_path
+
+    fill_in('name', :with => 'Leeroy')
+    fill_in('city', :with => 'Stormforge')
+    fill_in('state', :with => 'Iron Hills')
+    fill_in('avatar', :with => 'mrjenkins')
+    fill_in('birthdate', :with => '6/6/1999')
+    fill_in('date_hired', :with => '9/9/2001')
+    fill_in('department', :with => 'Raids')
+    click_on('Submit')
+
+    save_and_open_page
+
+    assert '/robots', current_path
+
+    assert page.has_content?('Leeroy')
+  end
+
+end
