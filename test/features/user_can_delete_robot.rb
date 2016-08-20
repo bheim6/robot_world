535e17f5748c91709ac7e38c590d60616d1cc71e
Brian Heim <bheim67@gmail.com>
2016-08-20 14:54:41 -0600

    added delete robot feature test

---
+++ test/features/user_can_delete_robot.rb
@@ -0,0 +1,45 @@
+require_relative '../test_helper'
+
+class UserCanDeleteRobotTest < FeatureTest
+
+  def test_user_can_delete_robot
+  visit '/'
+  click_link('New Robot')
+
+  fill_in('name', :with => 'Leeroy')
+  fill_in('city', :with => 'Stormforge')
+  fill_in('state', :with => 'Iron Hills')
+  fill_in('avatar', :with => 'mrjenkins')
+  fill_in('birthdate', :with => '6/6/1999')
+  fill_in('date_hired', :with => '9/9/2001')
+  fill_in('department', :with => 'Raids')
+  click_on('Submit')
+
+  click_link('New Robot')
+
+  fill_in('name', :with => 'Steve')
+  fill_in('city', :with => 'Stormforge')
+  fill_in('state', :with => 'Iron Hills')
+  fill_in('avatar', :with => 'steve')
+  fill_in('birthdate', :with => '6/6/1999')
+  fill_in('date_hired', :with => '9/9/2001')
+  fill_in('department', :with => 'Raids')
+  click_on('Submit')
+
+  assert page.has_content?('Leeroy')
+  assert page.has_content?('Steve')
+
+  click_on('Leeroy')
+
+  assert 'robots/1', current_path
+
+  click_on('Delete Robot')
+
+  assert '/robots', current_path
+
+  save_and_open_page
+
+  assert_equal false, page.has_content?('Leeroy')
+  assert page.has_content?('Steve')
+  end
+end
